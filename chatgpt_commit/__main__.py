f62dd8e9970aaefbff52384955329cfa91ebc123
Niels Lemmens <1936122+bulv1ne@users.noreply.github.com>
2023-07-11 12:59:31 +0200

    Refactored main function to include an argument parser and added error handling for authentication errors

--- chatgpt_commit/__main__.py
+++ chatgpt_commit/__main__.py
@@ -1,10 +1,22 @@
+import argparse
 import os
 import subprocess
+import sys
 
 import openai
 
+parser = argparse.ArgumentParser(prog="chatgpt-commit", description="ChatGPT Commit Message Generator")
+parser.add_argument(
+    "-t",
+    "--temperature",
+    type=float,
+    default=1.0,
+    help="set the temperature for the openai model",
+)
+
 
 def main():
+    args = parser.parse_args()
     openai.organization = os.getenv("OPENAI_ORG_ID")
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -13,23 +25,27 @@ def main():
 
     content = "\n\n".join(
         [
-            "Based on the following Git diff output, could you help me to create an appropriate Git commit message?",
+            "Based on the following Git diff output, could you help me to create an appropriate Git commit message in one line?",
             git_diff_output,
         ]
     )
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "user",
-                "content": content,
-            }
-        ],
-        temperature=0.6,
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": content,
+                }
+            ],
+            temperature=args.temperature,
+        )
+    except openai.error.AuthenticationError as e:
+        print(str(e), file=sys.stderr)
+        sys.exit(1)
 
     for choice in response.choices:
-        print(choice.message["content"])
+        print(choice.message.content)
 
 
 if __name__ == "__main__":
